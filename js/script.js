1a7a34d1b4116c92bb2265a5ccdc413b2f02ffd3
nicgiordani <nikgiordani@gmail.com>
2021-01-15 12:30:22 +0100

    terza parte + footer

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var app = new Vue(
     el: '#root',
     data: {
       links: ["Home", "About", "Services", "Blog", "Contacts", "Portfolio"],
+      quick: ["Services", "Blog", "Contacts"],
+      resources: ["Art Design", "Computer", "IT News", "Networking", "Web Security", "Portfolio"],
     },
     methods: {
 
